94c61aa00d6ebaeab69885534f8b18c53b11117d
Montassar Laribi <mohamedmontassar.laribi@esprit.tn>
2021-01-14 23:41:33 +0100

    Change import to link tag with preload

--- pages/_document.js
+++ pages/_document.js
@@ -39,6 +39,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang={lng}>
         <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400&display=swap"
+            rel="preload"
+          />
           <script
             async
             src={"https://www.googletagmanager.com/gtag/js?id=" + GA_ID}
