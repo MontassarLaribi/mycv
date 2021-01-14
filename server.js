f4b4b8c1e0bbd6fcb106b624f017f15c6be4a891
Montassar Laribi <mohamedmontassar.laribi@esprit.tn>
2021-01-14 21:47:30 +0100

    Https back

--- server.js
+++ server.js
@@ -8,33 +8,34 @@ const ports = {
   http: 80,
   https: 443,
 };
-// const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 const server = express();
 const httpsApp = express();
 
-// const options = {
-//   key: fs.readFileSync("./cert/localhost.key"),
-//   cert: fs.readFileSync("./cert/localhost.crt"),
-// };
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/montassarlaribi.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/montassarlaribi.com/cert.pem"),
+  ca: fs.readFileSync("/etc/letsencrypt/live/montassarlaribi.com/chain.pem"),
+};
 
 app.prepare().then(() => {
-  server.get("*", (req, res) => handle(req, res));
+  httpsApp.get("*", (req, res) => handle(req, res));
 
-  // server.get("*", (req, res) => {
-  //   res.writeHead(302, {
-  //     Location: "https://" + req.headers.host + req.url,
-  //   });
-  //   res.end();
-  // });
+  server.get("*", (req, res) => {
+    res.writeHead(302, {
+      Location: "https://" + req.headers.host + req.url,
+    });
+    res.end();
+  });
 
-  // https
-  //   .createServer(options, httpsApp)
-  //   .listen(ports.https, "0.0.0.0", (err) => {
-  //     if (err) throw err;
-  //     console.log(`> HTTPS Ready on https://localhost:${ports.https}`);
-  //   });
+  https
+    .createServer(options, httpsApp)
+    .listen(ports.https, "0.0.0.0", (err) => {
+      if (err) throw err;
+      console.log(`> HTTPS Ready on https://localhost:${ports.https}`);
+    });
 
   http.createServer(server).listen(ports.http, "0.0.0.0", (err) => {
     if (err) throw err;
