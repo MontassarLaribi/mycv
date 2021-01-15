61df36817bd2abd020f390d0c4b6ecc9ac604c21
Montassar Laribi <mohamedmontassar.laribi@esprit.tn>
2021-01-15 12:28:47 +0100

    add as attribute to preload link tag

--- pages/_document.js
+++ pages/_document.js
@@ -42,6 +42,7 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400&display=swap"
             rel="preload"
+            as="font"
           />
           <script
             async
